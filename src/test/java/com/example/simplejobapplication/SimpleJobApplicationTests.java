5a99f7b90bf3d02a63ad1f06e9f3ba382b5c6293
DoQuangAnh[Sotatek] <141228247+t2307530@users.noreply.github.com>
2023-11-20 17:50:39 +0700

    init

---
+++ src/test/java/com/example/simplejobapplication/SimpleJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplejobapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
